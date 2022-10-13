271843f5b8e74d792bad47a62f2e89b7438ed6b2
DistiAp <115688315+DistiAp@users.noreply.github.com>
2022-10-13 23:36:35 +0700

    Add files via upload

---
+++ 5_ModivierProtected_Disti.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace MyApplication
+{
+
+    class Person
+    {
+        protected string name = "Disti";
+    }
+
+    // derived class
+    class Program : Person
+    {
+        static void Main(string[] args)
+        {
+
+            // creating object of derived class
+            Program program = new Program();
+
+            // accessing name field and printing it
+            Console.WriteLine("Name: " + program.name);
+            Console.ReadLine();
+        }
+    }
+}
